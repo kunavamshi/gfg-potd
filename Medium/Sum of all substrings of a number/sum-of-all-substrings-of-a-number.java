55e36f016674725b72ca5958f00113cda968c42d
VAMSHI KUNA <118479150+kunavamshi@users.noreply.github.com>
2024-04-04 21:05:10 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of all substrings of a number/sum-of-all-substrings-of-a-number.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total testcases
+		int t=sc.nextInt();
+		sc.nextLine();
+		while(t-->0)
+		{
+		    //taking the String
+		    String s=sc.nextLine();
+		    Solution ob = new Solution();
+		    //calling method sumSubstrings() 
+		    System.out.println(ob.sumSubstrings(s));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find sum of all possible substrings of the given string.
+    public static long sumSubstrings(String s)
+    {
+    //Your code here
+        long ans=0,previous=0,mod=1000000007;
+        for(int i=0;i<s.length();i++){
+            long curr = (previous*10)%mod+(s.charAt(i)-'0')*(i+1)%mod;
+            previous=curr;
+            ans=(ans+curr)%mod;
+        }
+        return ans;
+    }
+}
