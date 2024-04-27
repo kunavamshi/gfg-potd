b7503b8acfe3d7aef5229a2e5e33140b21d0c8e7
VAMSHI KUNA <118479150+kunavamshi@users.noreply.github.com>
2024-04-28 01:35:38 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Delete Middle of Linked List/delete-middle-of-linked-list.py
@@ -0,0 +1,67 @@
+class Solution:
+    def deleteMid(self, head):
+        if not head or not head.next:  # If the linked list is empty or contains a single node
+            return None
+        
+        # Find the middle node using slow and fast pointers
+        slow = head
+        fast = head
+        prev = None
+        
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Delete the middle node
+        prev.next = slow.next
+        
+        return head
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Llist:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data, tail):
+        node = Node(data)
+
+        if not self.head:
+            self.head = node
+            return node
+
+        tail.next = node
+        return node
+
+
+def printList(head):
+    while head:
+        print(head.data, end=' ')
+        head = head.next
+    print()
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for tcs in range(t):
+        n=int(input())
+        arr1 = [int(x) for x in input().split()]
+        ll = Llist()
+        tail = None
+        for nodeData in arr1:
+            tail = ll.insert(nodeData, tail)
+        obj = Solution();
+        res=obj.deleteMid(ll.head)
+        printList(res)
+# } Driver Code Ends
