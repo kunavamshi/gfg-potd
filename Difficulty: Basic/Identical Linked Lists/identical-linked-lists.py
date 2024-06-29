26c44b755c604ee1f508e2fc511bd019c2108bb4
VAMSHI KUNA <118479150+kunavamshi@users.noreply.github.com>
2024-06-29 07:04:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Identical Linked Lists/identical-linked-lists.py
@@ -0,0 +1,75 @@
+# Node Class    
+class node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+# Function to check whether two linked lists are identical or not.
+def areIdentical(head1, head2):
+    # Traverse both lists simultaneously
+    while head1 is not None and head2 is not None:
+        # If the data in the current nodes do not match, return False
+        if head1.data != head2.data:
+            return False
+        # Move to the next nodes
+        head1 = head1.next
+        head2 = head2.next
+    
+    # If both lists have reached their end, they are identical
+    # If one of the lists still has nodes, they are not identical
+    return head1 is None and head2 is None
+
+
+
+#{ 
+ # Driver Code Starts
+# Node Class
+class node:
+
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+
+# Linked List Class
+class Linked_List:
+
+    def __init__(self):
+        self.head = None
+
+    def insert(self, val):
+        new_node = node(val)
+        new_node.data = val
+        new_node.next = self.head
+        self.head = new_node
+
+
+def printList(head):
+    while head:
+        print(head.data, end=' ')
+        head = head.next
+    print()
+
+
+def createList(arr, n):
+    lis = Linked_List()
+    for i in range(n):
+        lis.insert(arr[i])
+    return lis.head
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        head1 = createList(arr, n)
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        head2 = createList(arr, n)
+        if (areIdentical(head1, head2)):
+            print('true')
+        else:
+            print('false')
+
+# } Driver Code Ends
