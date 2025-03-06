ca0f8bf1ea050e35c7f064621b6f10ab662bbf00
VAMSHI KUNA <vamshikuna905@gmail.com>
2025-03-06 09:53:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.py
@@ -0,0 +1,32 @@
+class Solution:
+    def lcs(self, s1, s2):
+        m, n = len(s1), len(s2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        return dp[m][n]
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+import atexit
+import io
+import sys
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        s1 = str(input())  # Take first string as input
+        s2 = str(input())  # Take second string as input
+        ob = Solution()
+        # Call the lcs function and print the result
+        print(ob.lcs(s1, s2))
+        print("~")
+
+# } Driver Code Ends
